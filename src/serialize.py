60132ccc9f02621eb2f95b7e9ff0e8ac28a5cc90
DevyanaCT <chandthakuridevyana@gmail.com>
2025-10-29 12:12:05 +0900

    do i know?

--- src/serialize.py
+++ src/serialize.py
@@ -4,10 +4,37 @@ class Node:
         self.left = left
         self.right = right
 
+
 def serialize(root):
-    # TODO: preorder with '#' for None, space-separated
-    pass
+    """Preorder serialization with '#' for None, space-separated."""
+    vals = []
+
+    def dfs(node):
+        if node is None:
+            vals.append("#")
+            return
+        vals.append(str(node.val))
+        dfs(node.left)
+        dfs(node.right)
+
+    dfs(root)
+    return " ".join(vals)
+
 
 def deserialize(s):
-    # TODO: rebuild from tokens
-    pass
+    """Rebuild tree from preorder tokens."""
+    if not s:
+        return None
+
+    tokens = iter(s.split())
+
+    def dfs():
+        val = next(tokens)
+        if val == "#":
+            return None
+        node = Node(val)  # Keep as string
+        node.left = dfs()
+        node.right = dfs()
+        return node
+
+    return dfs()
